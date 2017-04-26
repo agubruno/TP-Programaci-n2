cdbc7a5db69546de10b092f9b231429c00573edb
ldidone <lucas_crar14@hotmail.com>
2017-04-26 18:38:01 -0300

    Actualizacion TP

---
+++ TrabajoPrácticoTRUCO/Carta.Test/CartaTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrabajoPrácticoTRUCO.Entidades;
+
+namespace TrabajoPrácticoTRUCO.Test
+{
+    [TestClass]
+    public class CartaTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void NoPermitirCrearCartaDuplicada()
+        {
+            var carta = Carta.CrearCarta();
+            //Arange
+
+            //    var carta1 = Carta. referenciar
+            //Act
+
+            //Assert -> Espera Excepcion
+
+        }
+    }
+}
---
+++ TrabajoPrácticoTRUCO/TrabajoPrácticoTRUCO.Entidades/Carta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoPrácticoTRUCO.Entidades
+{
+    public enum palos { Espada, Basto, Oro, Copa }
+    //public enum numeros -> ver
+
+    public class Carta
+    {
+        private Carta()
+        {
+            Cartas = new List<Carta>();
+        }
+
+        public palos Palo { get; set; }
+        public int Numero { get; set; }
+        private static List<Carta> Cartas { get; set; }
+
+        public static Carta CrearCarta(palos palo, int numero)
+        {
+            if ((numero >= 1 && numero <= 7) || (numero >= 10 && numero <= 12))
+            {
+                return Carta.ObtenerCarta(palo, numero);
+            }
+
+            throw new NotSupportedException("El valor ingresado no corresponde a un valor valido."); //ver tipo de excepcion          
+        }
+
+        private static Carta ObtenerCarta(palos palo, int numero) //ver si tiene que ser estatico
+        {
+            foreach (var carta in Cartas)
+            {
+                if (carta.Palo == palo && carta.Numero == numero)
+                {
+                    //throw new NotSupportedException("No se puede agregar un objeto duplicado.");
+                    return carta;
+                }
+            }
+            Carta nuevaCarta = new Carta();
+            nuevaCarta.Numero = numero;
+            nuevaCarta.Palo = palo;
+            Cartas.Add(nuevaCarta);
+            return nuevaCarta;
+        }
+    }
+}
