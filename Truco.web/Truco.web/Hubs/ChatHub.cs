d84084f7780af8f1f6eb48b2335a946de55162ba
ldidone <lucas_crar14@hotmail.com>
2017-05-03 16:50:43 -0300

    Truco web chat (SignalR)

---
+++ Truco.web/Truco.web/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Truco.web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void EnviarMensaje(string nombre, string texto)
+        {
+            if ((texto != "") && (nombre != "")) //PARA QUE ESTE CORRECTO DEBERÍA DISPARAR EVENTO CUANDO NO SE PUEDE ENVIAR
+            {
+                Clients.All.mostrarMensaje(nombre, texto);
+            }
+            
+        }
+
+        public void EnviarMensajePrivado(string nombre, string texto)
+        {
+            if ((texto != "") && (nombre != ""))
+            {
+                Clients.Caller.mostrarMensaje(nombre, texto);
+            }
+        }
+    }
+}
